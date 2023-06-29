0c56a0d041ac2a5e649bca21e9d0d17763717594
Nikita_Hnitetskyi <106909771+NikitaHnitetskyi@users.noreply.github.com>
2023-06-29 12:14:50 +0300

    update project

--- Models/Cinema.cs
+++ Models/Cinema.cs
@@ -11,8 +11,11 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Cinema logo")]
         public string Logo { get; set; }
+        [Display(Name = "Cinema Name")]
         public string Name { get; set; }
+        [Display(Name = "Cinema description")]
         public string Description { get; set; }
 
         //Reletionship
